3fe6ef528bcf86fd6fc4add93d776914e65f2bc1
Parra <hpbenitez7@gmail.com>
2023-10-05 18:41:28 -0300

    construindo parte de renderização

--- neomentora_fe/src/components/Card/index.tsx
+++ neomentora_fe/src/components/Card/index.tsx
@@ -1,24 +1,19 @@
-import styles from './Card.module.css';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import styles from './Card.module.css'; // Importe o módulo de estilos corretamente
 
 interface CardProps {
     title: string;
     image: string;
     linkTo: string;
+    onClick?: () => void;
 }
 
-const Card: React.FC<CardProps> = ({ title, image, linkTo }) => {
-    const navigate = useNavigate();
-
-    const handleCardClick = () => {
-        navigate(linkTo);
-    };
-
+const Card: React.FC<CardProps> = ({ title, image, linkTo, onClick }) => {
     return (
-        <div className={styles.card} onClick={handleCardClick}>
-            <img src={image} alt={title} className={styles.cardImage} />
+        <div className={`${styles.card} ${styles.cardContainer}`} onClick={onClick}>
+            <img className={styles.cardImage} src={image} alt={title} />
             <div className={styles.cardTitle}>
-                <h2>{title}</h2>
+                <h3>{title}</h3>
             </div>
         </div>
     );
---
+++ neomentora_fe/src/routes/Canditatese/NegocioScreen.tsx
@@ -0,0 +1,8 @@
+export default function NegocioScreen() {
+    return (
+        <div>
+            <h1>Tela de Negocio</h1>
+            {/* Adicione a lógica e a apresentação específicas da tela de mentor aqui */}
+        </div>
+    );
+}
