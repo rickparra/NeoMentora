02c9a9ea9e9b6b026f0d6fa78ab8beeab9db4945
Parra <hpbenitez7@gmail.com>
2023-10-05 22:23:35 -0300

    Update NegocioScreen.tsx

--- neomentora_fe/src/routes/Canditatese/Negocio/NegocioScreen.tsx
+++ neomentora_fe/src/routes/Canditatese/Negocio/NegocioScreen.tsx
@@ -1,9 +1,12 @@
 import { ChangeEvent, useState } from 'react';
 import styles from './negocio.module.css';
 import defaultImage from '../../../assets/Images/user1.png'; // Importe sua imagem padrão aqui
+import { SmallBusinessProps, registerMentor, registerSmallBusiness } from '../register';
 
 export default function NegocioScreen() {
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
+    const [smallBusiness, setsmallBusiness] = useState<SmallBusinessProps>()
+
 
     // Função para lidar com a seleção de uma imagem
     const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
@@ -23,6 +26,29 @@ export default function NegocioScreen() {
         }
     };
 
+    const onclickSmallBusiness = () => {
+        registerSmallBusiness({
+            name: "fulano de tal",
+            email: "fulano@gmail.com8",
+            ddd: 11,
+            phone: 123212321,
+            type_of_document: "CPF",
+            document: "12345678901",
+            activity_sector: "setor de atividade",
+            problem_or_challenge: "meu problema é ter problema",
+            photo_url: "https://sm.ign.com/ign_br/screenshot/default/imagem-2023-09-04-171030002_zt57.jpg"
+        })
+            .then(res => setsmallBusiness(res.data))
+            .catch(error => console.error(error))
+    }
+
+    
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault(); // Impede o comportamento padrão do formulário
+        // Realize o processamento do formulário aqui
+      };
+
     return (
         <div className={styles.negocio}>
             <div className={styles.top}>
@@ -33,85 +59,86 @@ export default function NegocioScreen() {
                 <p>Cadastre-se com suas informações, por favor.</p>
             </div>
 
-            <div className={styles.mid}>
-                <div className={styles.inputContainer}>
-                    <div className={styles.inputSection}>
-                        <div className={styles.inputGroup}>
-                            <label htmlFor="empresa">Nome da empresa</label>
-                            <input type="text" id="empresa" placeholder="Digite o nome da empresa" />
+            <form onSubmit={handleSubmit}>
+                <div className={styles.mid}>
+                    <div className={styles.inputContainer}>
+                        <div className={styles.inputSection}>
+                            <div className={styles.inputGroup}>
+                                <label htmlFor="empresa">Nome da empresa</label>
+                                <input type="text" id="empresa" placeholder="Digite o nome da empresa" />
+                            </div>
+                            <div className={styles.inputGroup}>
+                                <label htmlFor="cnpj">CPF ou CNPJ</label>
+                                <input type="text" id="cnpj" placeholder="Digite o CPF ou CNPJ" />
+                            </div>
+                            <div className={styles.inputGroup}>
+                                <label htmlFor="email">Email</label>
+                                <input type="email" id="email" placeholder="Digite o seu email" />
+                            </div>
+                            <div className={styles.inputGroup}>
+                                <label htmlFor="telefone">Telefone</label>
+                                <input type="text" id="telefone" placeholder="Digite o telefone" />
+                            </div>
                         </div>
-                        <div className={styles.inputGroup}>
-                            <label htmlFor="cnpj">CPF ou CNPJ</label>
-                            <input type="text" id="cnpj" placeholder="Digite o CPF ou CNPJ" />
-                        </div>
-                        <div className={styles.inputGroup}>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="email" placeholder="Digite o seu email" />
-                        </div>
-                        <div className={styles.inputGroup}>
-                            <label htmlFor="telefone">Telefone</label>
-                            <input type="text" id="telefone" placeholder="Digite o telefone" />
+
+                        <div className={styles.inputSection}>
+                            <div className={styles.inputGroup}>
+                                <label htmlFor="setor">Setor de atuação</label>
+                                <input type="text" id="setor" placeholder="Digite o setor de atuação" />
+                            </div>
+                            <div className={styles.inputGroup}>
+                                <label htmlFor="problema">Problema ou desafio enfrentado</label>
+                                <textarea id="problema" placeholder="Descreva o problema ou desafio"></textarea>
+                            </div>
+                            <div className={styles.inputGroup}>
+                                <label htmlFor="senha">Criar senha</label>
+                                <input type="password" id="senha" placeholder="Crie sua senha" />
+                            </div>
                         </div>
                     </div>
 
-                    <div className={styles.inputSection}>
-                        <div className={styles.inputGroup}>
-                            <label htmlFor="setor">Setor de atuação</label>
-                            <input type="text" id="setor" placeholder="Digite o setor de atuação" />
+                    <div className={styles.midFoto}>
+
+                        {/* Input de arquivo oculto */}
+                        <input
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageUpload}
+                            id="imageUploadInput"
+                            style={{ display: 'none' }}
+                        />
+
+                        {/* Div para exibir a imagem selecionada ou a imagem padrão */}
+                        <div className={styles.selectedImage}>
+                            {selectedImage ? (
+                                <img
+                                    src={selectedImage}
+                                    alt="Imagem de perfil"
+                                    className={styles.coverImage}
+                                />
+                            ) : (
+                                <img
+                                    src={defaultImage}
+                                    alt="Imagem padrão"
+                                    className={styles.coverImage}
+                                />
+                            )}
                         </div>
-                        <div className={styles.inputGroup}>
-                            <label htmlFor="problema">Problema ou desafio enfrentado</label>
-                            <textarea id="problema" placeholder="Descreva o problema ou desafio"></textarea>
-                        </div>
-                        <div className={styles.inputGroup}>
-                            <label htmlFor="senha">Criar senha</label>
-                            <input type="password" id="senha" placeholder="Crie sua senha" />
-                        </div>
+
+                        {/* Botão para selecionar a imagem */}
+                        <button className={styles.selectImageButton} onClick={handleSelectImageClick}>
+                            Selecionar Imagem
+                        </button>
                     </div>
                 </div>
 
-                <div className={styles.midFoto}>
-
-                    {/* Input de arquivo oculto */}
-                    <input
-                        type="file"
-                        accept="image/*"
-                        onChange={handleImageUpload}
-                        id="imageUploadInput"
-                        style={{ display: 'none' }}
-                    />
-
-                    {/* Div para exibir a imagem selecionada ou a imagem padrão */}
-                    <div className={styles.selectedImage}>
-                        {selectedImage ? (
-                            <img
-                                src={selectedImage}
-                                alt="Imagem de perfil"
-                                className={styles.coverImage}
-                            />
-                        ) : (
-                            <img
-                                src={defaultImage}
-                                alt="Imagem padrão"
-                                className={styles.coverImage}
-                            />
-                        )}
-                    </div>
-
-                    {/* Botão para selecionar a imagem */}
-                    <button className={styles.selectImageButton} onClick={handleSelectImageClick}>
-                        Selecionar Imagem
-                    </button>
+                <div className={styles.botton}>
+                    <label htmlFor="termos" className={styles.termos}>Termos e Condições</label>
+                    <input type="checkbox" name="termosCondicoes" id="termosCondicoes" className={styles.checked} />
                 </div>
-            </div>
 
-            <div className={styles.botton}>
-                <label htmlFor="termos" className={styles.termos}>Termos e Condições</label>
-                <input type="checkbox" name="termosCondicoes" id="termosCondicoes" className={styles.checked} />
-            </div>
-
-            <button className={styles.selectImageButton}>Registrar Conta</button>
-
+                <button className={styles.selectImageButton} onClick={()=>onclickSmallBusiness()}>Registrar Conta</button>
+            </form>
 
         </div>
     );
